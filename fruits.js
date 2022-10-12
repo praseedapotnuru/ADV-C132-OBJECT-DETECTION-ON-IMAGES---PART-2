00fad06e25cc75d36d4f5b4aeaa9c0cefbf00a6a
praseedapotnuru <82453459+praseedapotnuru@users.noreply.github.com>
2022-10-12 19:05:46 +0200

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,27 @@
+Status = "";
+image = "";
+
+function preload() {
+    image = loadImage("fruit basket.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    modal = ml5.objectDetector("cosossd", modalLoaded);
+    document.getElementById("status").innerHTML = "Detecting objects";
+}
+
+function modalLoaded() {
+    Status = true;
+    modal.detect(image, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.log(error);
+
+    }
+    console.log(results);
+    objects = results;
+}    
